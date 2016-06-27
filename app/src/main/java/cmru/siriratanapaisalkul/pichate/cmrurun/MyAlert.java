6be790721cc20923242124d84d85c9f0f36b1ec8
pichatesir <lpich@hotmail.com>
2016-06-27 16:34:47 +0700

    Finish 27 JUNE (Alert)

---
+++ app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MyAlert.java
@@ -0,0 +1,33 @@
+package cmru.siriratanapaisalkul.pichate.cmrurun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by PICHATE on 27/6/2559.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.danger);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+
+    }
+
+}   //Main Class
