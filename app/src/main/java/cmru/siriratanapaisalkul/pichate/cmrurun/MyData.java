c615caf6fbb724dbfefc504996087f2ceee2fc33
pichatesir <lpich@hotmail.com>
2016-06-28 11:18:35 +0700

    Confirm Data OK

---
+++ app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MyData.java
@@ -0,0 +1,18 @@
+package cmru.siriratanapaisalkul.pichate.cmrurun;
+
+/**
+ * Created by PICHATE on 28/6/2559.
+ */
+public class MyData {
+
+    //Explicit
+    private int[] avataInts = new int[]{R.drawable.bird48,
+            R.drawable.doremon48, R.drawable.kon48,
+            R.drawable.nobita48, R.drawable.rat48};
+
+    public int[] getAvataInts() {
+        return avataInts;
+    }
+
+
+}   // Main Class
