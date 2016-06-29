7b64e18a83c1e8f34cf0b0c61d987f2c8ffedea0
pichatesir <lpich@hotmail.com>
2016-06-29 15:38:49 +0700

    Get Location

--- app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MapsActivity.java
+++ app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MapsActivity.java
@@ -1,7 +1,13 @@
 package cmru.siriratanapaisalkul.pichate.cmrurun;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,18 +21,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private double cmruLatADouble = 18.8071096, cmruLngADouble=98.9844154;
+    private double userLatADouble , userLngADouble;
+    private LocationManager locationManager;
+    private Criteria criteria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
+
+        //Setup
+        userLatADouble = cmruLatADouble;
+        userLngADouble = cmruLngADouble;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }//Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+
+            userLatADouble = networkLocation.getLatitude();
+            userLngADouble = networkLocation.getLongitude();
+
+        }//if
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            userLatADouble = gpsLocation.getLatitude();
+            userLngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("29JUNEV1", "userLat ==>" + userLatADouble);
+        Log.d("29JUNEV1", "userLng ==>" + userLngADouble);
+
+    }   //OnResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
     }
 
+    public Location myFindLocation(String strProvider) {
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvider)) {
+            locationManager.requestLocationUpdates(strProvider,1000,10,locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
 
+        } else {
+            Log.d("29JUNE", "Cannot Find Location");
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            userLatADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+
+        }   //onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -38,7 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         createStationMarker();
 
 
-    }   //Main Method
+    }
 
     private void createStationMarker() {
 
