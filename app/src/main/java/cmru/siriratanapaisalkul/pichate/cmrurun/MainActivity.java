71fbd4e62f83c33332a639689c4f06bf60cbd01f
pichatesir <lpich@hotmail.com>
2016-06-29 13:33:00 +0700

    Setup CMRU

--- app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MainActivity.java
+++ app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MainActivity.java
@@ -101,7 +101,10 @@ public class MainActivity extends AppCompatActivity {
                 } else if (passwordString.equals(truePasswordString)) {
                     //Password True
                     Toast.makeText(context, "Welcome" + nameUserString,Toast.LENGTH_SHORT).show();
-                    Intent intent = new
+                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    intent.putExtra("Name", nameUserString);
+                    startActivity(intent);
+                    finish();
                 } else {
                     //Password false
                     MyAlert myAlert = new MyAlert();
