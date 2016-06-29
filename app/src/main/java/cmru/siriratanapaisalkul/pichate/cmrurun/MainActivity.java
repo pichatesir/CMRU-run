70e192b761a2aa9889976348ae4097b73682386f
pichatesir <lpich@hotmail.com>
2016-06-29 10:41:18 +0700

    Authen Finish

--- app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MainActivity.java
+++ app/src/main/java/cmru/siriratanapaisalkul/pichate/cmrurun/MainActivity.java
@@ -9,11 +9,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         //Explicit
         private Context context;
         private String strURL;
+        private boolean statusABoolean = true;
+        private String truePasswordString, nameUserString;
 
         public SynUser(Context context, String strURL) {
             this.context = context;
@@ -75,6 +81,38 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("29JUNE", "JSON ==>" + s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    if (userString.equals(jsonObject.getString("User"))) {
+                        statusABoolean = false;
+                        truePasswordString = jsonObject.getString("Password");
+                        nameUserString = jsonObject.getString("Name");
+
+                    }   //if
+
+                }   //for
+                if (statusABoolean) {
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "ไม่มี User นี้",
+                            "ไม่มี" + userString + "ในฐานข้อมูลของเรา");
+                } else if (passwordString.equals(truePasswordString)) {
+                    //Password True
+                    Toast.makeText(context, "Welcome" + nameUserString,Toast.LENGTH_SHORT).show();
+                } else {
+                    //Password false
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "Password False",
+                            "Please Try Again Password False");
+                }
+
+
+            } catch (Exception e) {
+                Log.d("29JUNE", "e onPost ==>" + e.toString());
+            }
+
 
         }   //onPost
 
